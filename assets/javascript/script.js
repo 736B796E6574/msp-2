4b5c0e4a4cbd88eb7ee948a5b0d1ca188d4e3211
martin-mcinerney <martin_mcinerney@live.ie>
2022-06-09 08:34:21 +0100

    Added startGame function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -87,3 +87,17 @@ const attachEventListeners = () => {
 
 newGame()
 attachEventListeners()
+
+// this disables the button and set the game in motion. This gets the clock ticking and updates the moves
+
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `time: ${state.totalTime} sec`
+    }, 1000)
+}
