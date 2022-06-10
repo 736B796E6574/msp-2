f91aca3e29d23ebe6c62bcb7737c0a0e8b437362
martin-mcinerney <martin_mcinerney@live.ie>
2022-06-10 17:18:17 +0100

    Added a media query

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -56,7 +56,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.");
     }
 
-    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍'];
+    const emojis = ['🦍', '🦜', '🐅', '🦁', '🐍', '🦒', '🦚', '🐼', '🐨', '🐸'];
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2);
     const items = shuffle([...picks, ...picks]);
     const cards = `
